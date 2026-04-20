b10e3085b77f5af61aff09b3987b107a9ba65f1f
Pavan Bedadham <pavanbedadham@Nehals-MacBook-Air.local>
2026-04-20 12:59:43 -0700

    public ip map

--- Infra/platform/main.tf
+++ Infra/platform/main.tf
@@ -14,9 +14,10 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                     = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24"]
+  map_public_ip_on_launch = true
 
   enable_nat_gateway     = true
   single_nat_gateway     = true
