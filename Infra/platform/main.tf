38e9bafd59dff63ac895f7938b7fe28b7444ab87
Pavan Bedadham <pavanbedadham@Nehals-MacBook-Air.local>
2026-04-20 15:26:02 -0700

    fixed destroy platform workflow

--- Infra/platform/main.tf
+++ Infra/platform/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "llm_gateway" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
